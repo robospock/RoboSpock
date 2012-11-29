5e1f215f83a58a293f8c4f18ca09bdba4d28eb02
Przemysław Jakubczyk <przemyslaw.jakubczyk@polidea.pl>
2012-11-29 10:42:52 +0100

    Removed useless test for RobolectricGuice annotation

--- TddAndroid/test/robolectric/src/test/groovy/pl/polidea/robospock/annotation/AnnotationsSpec.groovy
+++ TddAndroid/test/robolectric/src/test/groovy/pl/polidea/robospock/annotation/AnnotationsSpec.groovy
@@ -4,7 +4,6 @@ package pl.polidea.robospock.annotation
 import org.junit.runner.RunWith
 
 import pl.polidea.robospock.RoboSputnik
-//import pl.polidea.robospock.RobolectricGuiceModules
 import pl.polidea.robospock.UseShadows
 import pl.polidea.tddandroid.activity.MainActivity;
 import pl.polidea.tddandroid.module.TestTaskExecutorModule
@@ -31,37 +30,11 @@ abstract class AnnotationsSpec extends Specification{
 }
 
 @UseShadows(MyActivityManagerShadow)
-//@RobolectricGuiceModules([TestTaskExecutorModule,TestWebModule])
 @RunWith(RoboSputnik)
-class ShadowGuiceSputnikSpec extends AnnotationsSpec {
-}
-
-@UseShadows(MyActivityManagerShadow)
-@RunWith(RoboSputnik)
-//@RobolectricGuiceModules([TestTaskExecutorModule,TestWebModule])
-class ShadowSputnikGuiceSpec extends AnnotationsSpec{
+class ShadowSputnikSpec extends AnnotationsSpec {
 }
 
 @RunWith(RoboSputnik)
 @UseShadows(MyActivityManagerShadow)
-//@RobolectricGuiceModules([TestTaskExecutorModule,TestWebModule])
-class SputnikShadowGuiceSpec extends AnnotationsSpec{
-}
-
-@RunWith(RoboSputnik)
-//@RobolectricGuiceModules([TestTaskExecutorModule,TestWebModule])
-@UseShadows(MyActivityManagerShadow)
-class SputnikGuiceShadowSpec extends AnnotationsSpec{
-}
-
-//@RobolectricGuiceModules([TestTaskExecutorModule,TestWebModule])
-@UseShadows(MyActivityManagerShadow)
-@RunWith(RoboSputnik)
-class GuiceShadowSputnikSpec extends AnnotationsSpec{
-}
-
-//@RobolectricGuiceModules([TestTaskExecutorModule,TestWebModule])
-@RunWith(RoboSputnik)
-@UseShadows(MyActivityManagerShadow)
-class GuiceSputnikShadowSpec extends AnnotationsSpec{
+class SputnikShadowSpec extends AnnotationsSpec{
 }
