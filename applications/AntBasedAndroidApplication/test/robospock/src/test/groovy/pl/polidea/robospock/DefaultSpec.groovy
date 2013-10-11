b7a3de3b32f83cd8862d043ccdc940a9a84c50df
Stefano Dacchille <stefano.dacchille@gmail.com>
2013-10-11 22:33:46 +0200

    'Revert' spec in ant sample app modified by mistake

--- applications/AntBasedAndroidApplication/test/robospock/src/test/groovy/pl/polidea/robospock/DefaultSpec.groovy
+++ applications/AntBasedAndroidApplication/test/robospock/src/test/groovy/pl/polidea/robospock/DefaultSpec.groovy
@@ -1,34 +1,33 @@
 package pl.polidea.robospock
 
 import pl.polidea.robospock.activity.MainActivity
-//import pl.polidea.robospock.shadow.MyActivityManagerShadow
+import pl.polidea.robospock.shadow.MyActivityManagerShadow
 
-//@Config(shadows = [MyActivityManagerShadow])
+@Config(shadows = [MyActivityManagerShadow])
 class DefaultSpec extends RoboSpecification {
 
-  def "should inject view using RoboGuice"() {
-    given:
-    def mainActivity = new MainActivity()
-    mainActivity.onCreate(null)
+    def "should inject view using RoboGuice"() {
+        given:
+        def mainActivity = new MainActivity()
+        mainActivity.onCreate(null)
 
-    when:
-    def text = mainActivity.helloTv.text
+        when:
+        def text = mainActivity.helloTv.text
 
-    then:
-    text == "Hello Szlif!"
-  }
+        then:
+        text == "Hello Szlif!"
+    }
 
-  def "should have 0MB memory class"() {
-    given:
-    def mainActivity = new MainActivity()
-    mainActivity.onCreate(null)
+    def "should have 0MB memory class"() {
+        given:
+        def mainActivity = new MainActivity()
+        mainActivity.onCreate(null)
 
-    when:
-    def text = mainActivity.memoryTv.text
+        when:
+        def text = mainActivity.memoryTv.text
 
-    then:
-//    text == "I have 16 MB"
-    true
-  }
+        then:
+        text == "I have 16 MB"
+    }
 
 }
