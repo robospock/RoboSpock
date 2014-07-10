4b59cc480d1726b9e49d6b9fea58919f87c82985
Aleksander Piotrowski <pelotasplus@gmail.com>
2014-07-10 19:55:09 +0200

    zap code/master branch leftovers from gh-pages branch

--- robospock/src/main/groovy/pl/polidea/robospock/RoboSpecification.groovy
+++
@@ -1,8 +0,0 @@
-package pl.polidea.robospock
-
-import org.junit.runner.RunWith
-import pl.polidea.robospock.internal.RoboSputnik
-import spock.lang.Specification
-
-@RunWith(RoboSputnik)
-class RoboSpecification extends Specification {}
